42528867f04c24c5c8f3aea0ff7b81c7ee65026b
MarcusCaldeira <mac.caldr@gmail.com>
2019-02-26 15:16:44 -0600

    OK this is requiring the second script page.

--- notesNode/app.js
+++ notesNode/app.js
@@ -1,5 +1,11 @@
-console.log('Starting APP');
+console.log('Starting app.js');
 
 const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
 
-fs.appendFileSync('greetings.txt', 'Hello World');
+
+var user = os.userInfo();
+
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username} !`);
