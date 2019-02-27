efade007f48eeb5639ad5537afc9ff91bd5a2cdd
MarcusCaldeira <mac.caldr@gmail.com>
2019-02-26 19:20:30 -0600

    ok so I have and else if block that decalres the commands... the next step is getting specific information from the actual app.

--- notesNode/app.js
+++ notesNode/app.js
@@ -5,6 +5,18 @@ const _ = require('lodash');
 // Local Requirements- Modules that ive created and am requiring.
 const notes = require('./notes.js');
 
+var command = process.argv[2];
+console.log('Command:', command);
 
-var filteredArray = _.uniq(['Mike']);
-console.log(filteredArray);
+if (command === 'add'){
+    console.log('adding new note'); 
+} else if(command === 'list'){
+    console.log('listing all notes');
+} else if(command === 'read'){
+    console.log('reading');
+} else if (command === 'remove'){
+    console.log('Removing Note');
+}else {
+    console.log('command not recognized');
+} 
+
