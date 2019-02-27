b7f05163a544922deb1cf49888d15550fb2295f4
MarcusCaldeira <mac.caldr@gmail.com>
2019-02-26 18:03:58 -0600

    deleted the node package manager.

--- notesNode/app.js
+++ notesNode/app.js
@@ -2,10 +2,14 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
-const _ = require(lodash);
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result', notes.add(9, -2));
+
+var filteredArray = _.uniq(['Marcus', 1, 'Marcus',1,3,4,5]);
+console.log(filteredArray);
+// console.log(_.isString(true));
+// console.log(_.isString('Andrewnode'));
 
 
 // var user = os.userInfo();
