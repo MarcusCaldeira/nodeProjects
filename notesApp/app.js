22684d5458e496eb1f89774e2e0ae183ef0a3f4d
MarcusCaldeira <mac.caldr@gmail.com>
2019-03-05 10:12:44 -0600

    why do I have two working trees!

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt','My Name is Marcus');
+// fs.writeFileSync('notes.txt','My Name is Marcus');
+
+fs.appendFileSync('notes.txt', 'This is my second message');
