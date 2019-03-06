430bd5dac6653a8021aac8e57a9a9b42a3bfcf5d
MarcusCaldeira <mac.caldr@gmail.com>
2019-03-06 17:02:43 -0600

    alright that was working with JSON.

--- notesApp/playground/1-json.js
+++ notesApp/playground/1-json.js
@@ -1,14 +1,46 @@
 const fs = require('fs');
 
+//Load The Data IN
+const dataBuffer = fs.readFileSync('1-json.json');
+//Convert the data into a string. 
+const dataJSON = dataBuffer.toString();
+//Parse the data into JSON data. 
+const user = JSON.parse(dataJSON);
 
-const book = {
-    title: 'Ego Is the Enemy',
-    author: 'Ryan Holloway'
-}
+//Changed the data in the JSON Parse. 
+user.name =('Marcus');
+user.age = (28);
+
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
+
+
+
+
+
+
+
+
+//  //Read the file getting binary data
+// const dataBuffer = fs.readFileSync('1-json.json')
+// // turned the binary data into a string in js. 
+// const dataJson = dataBuffer.toString()
+// //Then parsed string data into json data,
+// const data = JSON.parse(dataJson);
+// //Then we accessed it by using properties of that object.  
+// console.log(data.title);
+
+
+// const book = {
+//     title: 'Ego Is the Enemy',
+//     author: 'Ryan Holloway'
+// }
 
  
-const bookJSON = JSON.stringify(book);
-fs.writeFileSync('1-json.json', bookJSON);
+// const bookJSON = JSON.stringify(book);
+// //Writes to the file
+// fs.writeFileSync('1-json.json', bookJSON);
 
 
 
