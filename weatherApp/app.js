8381f510c10168306acd701deafb4df2a73ec69e
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-01 12:36:11 -0500

    this will print  to the console the current temp and the chance of rain.

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -3,8 +3,7 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/c5d46e1b8a00c3c1f9d3790c5b3ee73d/37.8267,-122.4233'
 // The first argument is the URL(declared above in const url, the second is the function you are going to run. )
-request({url:url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({url:url,json:true}, (error, response) => {
+    console.log('It is Currently',response.body.currently.temperature + ' and there is a ',response.body.currently.precipProbability+ ' percent chance of rain')
 
 })
