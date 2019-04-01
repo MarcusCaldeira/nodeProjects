7f971fabc20eaffa28ac5ae14939b9e6fe242317
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-01 16:44:59 -0500

    ok i changed the lat and the long so now im working from the saint paul lat and long. Now this project is local.

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -4,7 +4,7 @@ const request = require('request')
 
 
 //Setting Up Query String- This is where the ? followed by the key value pairs come from.
-const url = 'https://api.darksky.net/forecast/c5d46e1b8a00c3c1f9d3790c5b3ee73d/37.8267,-122.4233?key=value&otherKey=otherValue'
+const url = 'https://api.darksky.net/forecast/c5d46e1b8a00c3c1f9d3790c5b3ee73d/44.9537,93.0900'
 // The first argument is the URL(declared above in const url, the second is the function you are going to run. )
 request({url:url,json:true}, (error, response) => {
     let JSON = response.body.currently
