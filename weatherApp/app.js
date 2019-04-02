a37070a99dfa964400bed51fdc82409730c7fcb4
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-01 20:21:44 -0500

    ok the geocoding api has been called and im gonna look into creating error handling within the app itself.

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -8,6 +8,14 @@ const url = 'https://api.darksky.net/forecast/c5d46e1b8a00c3c1f9d3790c5b3ee73d/4
 // The first argument is the URL(declared above in const url, the second is the function you are going to run. )
 request({url:url,json:true}, (error, response) => {
     let JSON = response.body.currently
-    console.log('It is Currently',JSON.temperature + ' and there is a ',JSON.precipProbability+ ' percent chance of rain')
+    console.log(response.body.daily.data[0].summary + ' It is Currently',JSON.temperature + ' and there is a ',JSON.precipProbability+ ' percent chance of rain')
 
+})
+
+//Setting up the Geocoding API, so that users can search through a database to find what they need. 
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFyY3VzNTUxMDQiLCJhIjoiY2p0ejBwbjNpMWMycjQzbzV3cjQ2eDAxaSJ9.SgG3t2jLCpWeKxWfZGwVkg&limit=1'
+request({url:geocodeURL,json:true}, (error, response) => {
+    const longitude = response.body.features[0].center[0]
+    const latitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
 })
