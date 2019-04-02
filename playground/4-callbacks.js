1eae921e25d9fe5ce3a780eb1ebce8891ff946b2
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-02 11:35:05 -0500

    wow callbacks, are hard to understand. Im gonna have to work on this more and more often.

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,24 +1,38 @@
-//The callback function in this is the function we define and set into setTimeout
-setTimeout(()=> {
-    console.log('YOO')
-},2000)
+// //The callback function in this is the function we define and set into setTimeout
+// setTimeout(()=> {
+//     console.log('YOO')
+// },2000)
 
-const names = ['Andrew', 'Derrick', 'Ed']
-const shortNames = names.filter((name) => {
-    return name.length <= 5 
-})
-console.log(shortNames)
+// const names = ['Andrew', 'Derrick', 'Ed']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 5 
+// })
+// console.log(shortNames)
 
-//If our function is no longer synchronus and we call the callback with the value we want to send back. 
-const geocode = (address, callback) => {
+// //If our function is no longer synchronus and we call the callback with the value we want to send back. 
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitue: 0, 
+//             longitude: 0, 
+//         }
+//         callback(data)
+//     }, 2000)
+// }
+// geocode('Albuquerque', (data) => {
+//     console.log(data)
+// })
+
+
+
+//Define an add function that accepts the correct arguments
+const add = (a, b, callback) => {
+//Use setTimeout to simulate a 2 second delay
     setTimeout(() => {
-        const data = {
-            latitue: 0, 
-            longitude: 0, 
-        }
-        callback(data)
+        callback( a * b)
     }, 2000)
 }
-geocode('Albuquerque', (data) => {
-    console.log(data)
+//After 2 seconds are up, call the callback function with the sum
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
 })
