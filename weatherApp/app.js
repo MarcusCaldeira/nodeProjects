d0fbd1e44cb7ffece2f585cf800e9f526b2712bc
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-01 20:55:29 -0500

    doing some error handling, with things failing at lower level, inout, if it all works then we get the weather for the location

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -7,15 +7,22 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/c5d46e1b8a00c3c1f9d3790c5b3ee73d/44.9537,93.0900'
 // The first argument is the URL(declared above in const url, the second is the function you are going to run. )
 request({url:url,json:true}, (error, response) => {
-    let JSON = response.body.currently
-    console.log(response.body.daily.data[0].summary + ' It is Currently',JSON.temperature + ' and there is a ',JSON.precipProbability+ ' percent chance of rain')
-
+    if (error){
+        console.log('The weather service cant be reached...right now!')
+    } else if (response.body){
+        console.log('Unable to find the location')
+    }
+    else{
+        let JSON = response.body.currently
+        console.log(response.body.daily.data[0].summary + ' It is Currently',JSON.temperature + ' and there is a ',JSON.precipProbability+ ' percent chance of rain')
+    }
+    
 })
 
-//Setting up the Geocoding API, so that users can search through a database to find what they need. 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFyY3VzNTUxMDQiLCJhIjoiY2p0ejBwbjNpMWMycjQzbzV3cjQ2eDAxaSJ9.SgG3t2jLCpWeKxWfZGwVkg&limit=1'
-request({url:geocodeURL,json:true}, (error, response) => {
-    const longitude = response.body.features[0].center[0]
-    const latitude = response.body.features[0].center[1]
-    console.log(latitude, longitude)
-})
+// //Setting up the Geocoding API, so that users can search through a database to find what they need. 
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFyY3VzNTUxMDQiLCJhIjoiY2p0ejBwbjNpMWMycjQzbzV3cjQ2eDAxaSJ9.SgG3t2jLCpWeKxWfZGwVkg&limit=1'
+// request({url:geocodeURL,json:true}, (error, response) => {
+//     const longitude = response.body.features[0].center[0]
+//     const latitude = response.body.features[0].center[1]
+//     console.log(latitude, longitude)
+// })
