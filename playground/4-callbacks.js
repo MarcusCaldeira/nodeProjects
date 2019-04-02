3eac34644f37684400b79285ac53233be8212c5c
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-02 10:42:36 -0500

    I'm gonna challenge myself with a problem in a second.

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,5 +1,24 @@
 //The callback function in this is the function we define and set into setTimeout
 setTimeout(()=> {
     console.log('YOO')
-
 },2000)
+
+const names = ['Andrew', 'Derrick', 'Ed']
+const shortNames = names.filter((name) => {
+    return name.length <= 5 
+})
+console.log(shortNames)
+
+//If our function is no longer synchronus and we call the callback with the value we want to send back. 
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitue: 0, 
+            longitude: 0, 
+        }
+        callback(data)
+    }, 2000)
+}
+geocode('Albuquerque', (data) => {
+    console.log(data)
+})
