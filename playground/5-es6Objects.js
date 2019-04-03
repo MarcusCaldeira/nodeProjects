8591594e1af5fca578b970b747f2339f1df5297c
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 11:48:39 -0500

    this is cool ive started to debug on my own, im finally learning the problem solving skills that I need to get this going again.

--- playground/5-es6Objects.js
+++ playground/5-es6Objects.js
@@ -1,21 +1,39 @@
 // Object Property Shorthand
 
-const name = 'Andrew'
-const userAge = 27
+// const name = 'Andrew'
+// const userAge = 27
 
-const user = {
-    name, 
-    age: userAge,
-    location: 'Philedelphia'
-}
-console.log(user);
+// const user = {
+//     name, 
+//     age: userAge,
+//     location: 'Philedelphia'
+// }
+// console.log(user);
 
 
 //Object Destructing - When you have an object and youre trying to access properties from it. The goal is to destruct object properties into their variables. 
 const product = {
-    label: 'RednoteBook',
+    label: 'Red Notebook',
     price: 3,
     stock: 201, 
-    salePrice: undefined
+    salePrice: undefined,
+    rating: 4.2
 }
-console.log(product)
+// //Non desctucting syntax - will create WAY TO MUCH CODE
+// const label = product.label
+// const stock = product.stock
+// console.log(label, stock)
+
+
+
+// //To rename something use the colon, you can even assign things that arent in the original object by using an equal sign.
+// const {label: productLabel, stock, rating = 5} = product
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock)
+
+}
+transaction('Order', product)
