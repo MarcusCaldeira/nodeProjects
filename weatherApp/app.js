9e808490e54c3b35c640c3971532981a47074f2e
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 09:07:16 -0500

    first commit of the day!

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,36 +1,25 @@
 const request = require('request')
 
+//Iimporting files from this project. 
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 
 
-
-// //Setting Up Query String- This is where the ? followed by the key value pairs come from.
-// const url = 'https://api.darksky.net/forecast/c5d46e1b8a00c3c1f9d3790c5b3ee73d/44.9537,93.0900'
-// // The first argument is the URL(declared above in const url, the second is the function you are going to run. )
-// request({url:url,json:true}, (error, response) => {
-//     if (error){
-//         console.log('The weather service cant be reached...right now!')
-//     } else if (response.body.error){
-//         console.log('The WEATHER DATABSE IS DOWN RIGHT NOW!')
-//     }
-//     else{
-//         let JSON = response.body.currently
-//         console.log(response.body.daily.data[0].summary + ' It is Currently',JSON.temperature + ' and there is a ',JSON.precipProbability+ ' percent chance of rain')
-//     }
-    
-// })
-
-
-
 geocode('Saint Paul', (error, data) => {
-    console.log('Error', error)
-    console.log('data', data)
+    if (error){
+        console.log('Something went wrong:(')
+
+    } else{
+        console.log('Error', error)
+        console.log('data', data)
+        //Grabs the data from geocode to print forecast from it. 
+        forecast(data.latitude, data.longitude, (error, data) => {
+            console.log('Error', error)
+            console.log('Data', data)
+          })
+
+    }
 })
 
 
-forecast(44.9504, -93.0900, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
