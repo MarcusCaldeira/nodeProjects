e114030d66a02e7d2eafd30918c1547161327769
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 09:19:27 -0500

    im going to allow the user to choose what city theyre talking about via command line, with process.argv

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -5,18 +5,24 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 
+console.log(process.argv)
+
 
 geocode('Saint Paul', (error, data) => {
     if (error){
-        console.log('Something went wrong:(')
-
+        console.log(error)
+        //Alt-I could use return as it would stop function execution. 
     } else{
         console.log('Error', error)
         console.log('data', data)
         //Grabs the data from geocode to print forecast from it. 
-        forecast(data.latitude, data.longitude, (error, data) => {
-            console.log('Error', error)
-            console.log('Data', data)
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                console.log(error)
+            }  else {
+               console.log(data.location)
+               console.log(forecastData)
+            }
           })
 
     }
