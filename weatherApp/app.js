b93945e3b31277420764537d9581088b5c4236c0
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 12:06:43 -0500

    ok this works and app.js is refactored with es6 destructing tactics

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -7,21 +7,21 @@ const forecast = require('./utils/forecast')
 //This creates the variable that is the argument passed through the command line.
 const address = process.argv[2]
 if (!address){
-    console.log('that cant be blank!')
+    console.log('that cant be blank')
 } else{
-    geocode(address, (error, data) => {
+    //Callback called with two arguments - Data was an OBJECT, it is now destructed. 
+    geocode(address, (error, {latitude, longitude, location}) => {
         if (error){
             console.log(error)
             //Alt-I could use return as it would stop function execution. 
         } else{
             console.log('Error', error)
-            console.log('data', data)
             //Grabs the data from geocode to print forecast from it. 
-            forecast(data.latitude, data.longitude, (error, forecastData) => {
+            forecast(latitude,longitude, (error, forecastData) => {
                 if (error) {
                     console.log(error)
                 }  else {
-                   console.log(data.location)
+                   console.log(location)
                    console.log(forecastData)
                 }
               })
