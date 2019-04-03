549011e212aee859d412d104022d17338821888d
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 10:17:18 -0500

    but it all into an if statement. m

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -7,28 +7,29 @@ const forecast = require('./utils/forecast')
 //This creates the variable that is the argument passed through the command line.
 const address = process.argv[2]
 if (!address){
-    console.log('UMMM where is that?')
+    console.log('that cant be blank!')
+} else{
+    geocode(address, (error, data) => {
+        if (error){
+            console.log(error)
+            //Alt-I could use return as it would stop function execution. 
+        } else{
+            console.log('Error', error)
+            console.log('data', data)
+            //Grabs the data from geocode to print forecast from it. 
+            forecast(data.latitude, data.longitude, (error, forecastData) => {
+                if (error) {
+                    console.log(error)
+                }  else {
+                   console.log(data.location)
+                   console.log(forecastData)
+                }
+              })
+    
+        }
+    })
 }
 
 
-geocode(address, (error, data) => {
-    if (error){
-        console.log(error)
-        //Alt-I could use return as it would stop function execution. 
-    } else{
-        console.log('Error', error)
-        console.log('data', data)
-        //Grabs the data from geocode to print forecast from it. 
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
-            if (error) {
-                console.log(error)
-            }  else {
-               console.log(data.location)
-               console.log(forecastData)
-            }
-          })
-
-    }
-})
 
 
