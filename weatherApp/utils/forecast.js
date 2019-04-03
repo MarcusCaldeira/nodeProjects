73a18a28edf90411ed9619bfd48a9c5646aac798
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-03 15:32:57 -0500

    ok im gonna take a break on working with node for the day. m

--- weatherApp/utils/forecast.js
+++ weatherApp/utils/forecast.js
@@ -7,14 +7,13 @@ const request = require('request')
 // 3. The forecast function should have three potential calls to callback
 const forecast = (latitude,longitude, callback) => {
     const url = ('https://api.darksky.net/forecast/c5d46e1b8a00c3c1f9d3790c5b3ee73d/' + latitude + ',' +longitude)
-    request({url: url, json:true}, (error, response) => {
+    request({ url, json:true}, (error, { body }) => {
         if(error){
             callback('Unable to reach location services', undefined)
-        } else if (response.body.error){
+        } else if (body.error){
             callback('Unable to find the weather there', undefined)
         }else{
-            callback(undefined, {
-                data:response.body.daily.data[1].summary + ' It is Currently '+ response.body.currently.temperature + ' and there is a ' + response.body.currently.precipProbability + ' percent chance of rain'
+            callback(undefined, {data:body.daily.data[1].summary + ' It is Currently '+ body.currently.temperature + ' degrees F '+' and there is a ' + body.currently.precipProbability + ' percent chance of rain'
             })
         }
     })
