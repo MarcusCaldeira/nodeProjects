1ba1f7bcb4cac6c14f7c0c471d44458b658e9163
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-08 16:35:43 -0500

    we have a web server up and running! YAYAYAYA

--- webServer/src/app.js
+++ webServer/src/app.js
@@ -1,1 +1,18 @@
 const express = require('express')
+
+const app = express()
+
+/*
+The app.get takes in the /route and then takes in a 
+function
+(req = Request contains information about incoming request to server)
+(res = Response )
+  */ 
+app.get('', (req,res) => {
+    res.send('Hello Express!')
+})
+
+
+app.listen(2000, () => {
+    console.log('Server DID START')
+})
