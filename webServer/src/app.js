8aea8a12c753e90d0c372be87d6c939862065b94
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-08 19:22:14 -0500

    ok ive added two more paths, I can now start figuring out how to serve those two things up by using express.

--- webServer/src/app.js
+++ webServer/src/app.js
@@ -19,24 +19,6 @@ function
 (req = Request contains information about incoming request to server)
 (res = Response )
   */ 
-app.get('', (req,res) => {
-    res.send('<h1>WEATHER</h1>')
-})
-//Create the Help Page Route.
-app.get('/help', (req,res) => {
-    res.send([{
-        name:'Marcus', 
-        age: 27
-    },{
-        name: 'Sarah',
-        age: 28
-    }])
-})
-//Create the title page. 
-app.get('/about', (req,res) => {
-    res.send('<h1>About</h1>')
-})
-//Create the weather page.
 app.get('/weather', (req, res) => {
     res.send({
         location: 'Saint Paul',
