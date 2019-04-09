9ee03aecc21e2ca1ffa418e19a20e41a842e7cf5
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-08 19:08:29 -0500

    we are displaying the contents from the static html folder.

--- webServer/src/app.js
+++ webServer/src/app.js
@@ -1,6 +1,17 @@
+const path = require('path')
+
 const express = require('express')
 
+
+
+
 const app = express()
+//Generate the path to the public folder. 
+const publicDirectoryPath = path.join(__dirname,'../public')
+
+
+//This serves up static html, by using express and path(given to use by node)
+app.use(express.static(publicDirectoryPath))
 
 /*
 The app.get takes in the /route and then takes in a 
