8c3dfab9b601f799bf7b2e12fccbcaa4555ac705
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-23 14:17:25 -0500

    the fetch is now wired to the terminal.

--- webServer/public/js/app.js
+++ webServer/public/js/app.js
@@ -2,14 +2,7 @@ console.log('Client Side Javascript')
 
 
 
-fetch('http://localhost:2000/weather?adress=Boston').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-        } else
-        console.log(data)
-    })
-})
+
 
 const weatherForm =document.querySelector('form')
 const searchElement =document.querySelector('input')
@@ -18,5 +11,12 @@ const searchElement =document.querySelector('input')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = searchElement.value
-    console.log(location)
+    fetch('http://localhost:2000/weather?adress='+location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else
+                console.log(data)
+        })
+    })
 })
