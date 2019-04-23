14f09b48bf8df68b6eca04a47e482faa549a35b8
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-23 12:49:19 -0500

    the search input will be subitted to the console, now I have a feeling we will manipulate the query string in order to complete the search terms

--- webServer/public/js/app.js
+++ webServer/public/js/app.js
@@ -10,3 +10,13 @@ fetch('http://localhost:2000/weather?adress=Boston').then((response) => {
         console.log(data)
     })
 })
+
+const weatherForm =document.querySelector('form')
+const searchElement =document.querySelector('input')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = searchElement.value
+    console.log(location)
+})
