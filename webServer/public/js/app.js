d9da08612aaa4dc6a23e1dc722736c2ebce80923
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-23 09:54:23 -0500

    removed the other fetch call

--- webServer/public/js/app.js
+++ webServer/public/js/app.js
@@ -1,11 +1,6 @@
 console.log('Client Side Javascript')
 
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
 
 fetch('http://localhost:2000/weather?adress=Boston').then((response) => {
     response.json().then((data) => {
