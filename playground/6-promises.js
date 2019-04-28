7f447d598a795907fe103120637d06732c29de97
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-28 18:23:59 -0500

    so basically what makes promises so effective is that its hard to mess up only having two functions to call on makes things simple

--- playground/6-promises.js
+++ playground/6-promises.js
@@ -2,6 +2,7 @@ const doWorkPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         // resolve([1,7,10])
         reject('Things went wrong')
+        resolve([2,3,2])
     }, 2000)
 })
 
