89478a3e0750515b9bc4ab9cbbdd35f119e0730d
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-28 18:08:42 -0500

    so resolve and reject are two different things, also there are methods that we can use while on the terminal

--- playground/6-promises.js
+++ playground/6-promises.js
@@ -1,10 +1,12 @@
 const doWorkPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve([1,7,10])
+        // resolve([1,7,10])
+        reject('Things went wrong')
     }, 2000)
 })
 
 doWorkPromise.then((result) => {
     console.log('Success',result)
-
+}).catch((error) => {
+    console.log('error', error)
 })
