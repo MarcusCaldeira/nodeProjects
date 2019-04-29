4a129082a69801361ca7d9359939a96add3e67af
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-29 18:13:54 -0500

    created my first task for the application it should be available to view in my robot3t gui

--- taskManager/src/db/mongoose.js
+++ taskManager/src/db/mongoose.js
@@ -16,13 +16,32 @@ const User = mongoose.model(('User'), {
     }
 })
 
-const me = new User ({
-    name:'Marcus',
-    age: 27 
+// const me = new User ({
+//     name:'Marcus',
+//     age: 'mike'
+// })
+
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((error) =>{
+//     console.log('There was an error' + error)
+// })
+const Task = mongoose.model(('tasks'), {
+    description:{
+        type:String
+    },
+    completed:{
+        type:Boolean
+    }
 })
 
-me.save().then((me) => {
-    console.log(me)
-}).catch((error) =>{
-    console.log('There was an error' + error)
+const task = new Task({
+    description:'This is my first Task',
+    completed:false
+})
+
+task.save().then((task) => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
 })
