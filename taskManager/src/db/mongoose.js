6be64fa246d92b6c0fc3a530b0abf4e5c2eb7168
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-29 16:16:03 -0500

    setting up mongoose, gonna need to add mongoDB into the project

--- taskManager/src/db/mongoose.js
+++ taskManager/src/db/mongoose.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose')
+//This will connect to the database. URL+PORT+PORT & Options Object 
+mongoose.connect('mongodb://127.0.1:27017/taskManagerApi', {
+    useNewUrlParser: true,
+    //this ensures data is accessible
+    useCreateIndex:true
+})
+//Takes two arguments(stringNameOfModel,definition-the fields we want)
+const User = mongoose.model(('User'), {
+    //Set:{valueAnObject}
+    name:{},
+    age:{}
+
+})
