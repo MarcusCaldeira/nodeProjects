ab90e105a3ca3e45257c73f0b8d9307367630b8e
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-29 12:39:25 -0500

    there we go Ive started to seperate the files, im going to look into the process of updating the other files to refelct the new directory structure.

---
+++ mongoDbCreating/delete/deleteMany.js
@@ -0,0 +1,34 @@
+//Create Read Update and Delete - the Crud Operations
+
+// const mongodb = require('mongodb')
+// // The MongoClient gives us access to the function nessecary to complete the crud operations. 
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+//This is a shorthand for grabbing things with MongoDB
+const { MongoClient, ObjectID } = require('mongodb')
+
+
+//Using Mongos Protocol://IP Adress(localhost gets weird):Port Number
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const dataBaseName = 'task-manager'
+
+
+/*MongoClient to connect to server.the only method(ConnectionURL,
+    OptionsObject{useNewURlParser:true},
+    Callback Function that gets called when connected to the database) */
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error,client) => {
+    if(error){
+       return console.log('Unable to Connect from Database')
+    }
+    //gives a database refrence, so that we can use it in the code.
+    const db = client.db(dataBaseName)
+
+    db.collection('users').deleteMany({
+        age: 27
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(eror)
+    })
+})
