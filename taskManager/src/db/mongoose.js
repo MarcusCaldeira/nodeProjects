5e7b2485c505075d68afe73e018b406229184519
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-29 17:14:26 -0500

    ok so my type was wrong thats why the is wasnt printing to the console, happy my MongoDb db server is still running

--- taskManager/src/db/mongoose.js
+++ taskManager/src/db/mongoose.js
@@ -12,7 +12,17 @@ const User = mongoose.model(('User'), {
         type:String
     },
     age:{
-        age:Number
+        type:Number
     }
+})
 
+const me = new User ({
+    name:'Marcus',
+    age: 27 
+})
+
+me.save().then((me) => {
+    console.log(me)
+}).catch((error) =>{
+    console.log('There was an error' + error)
 })
