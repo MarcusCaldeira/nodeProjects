c23fc81e449ac788e90ee3a190ea8a1bf8fe4052
MarcusCaldeira <mac.caldr@gmail.com>
2019-04-30 10:29:00 -0500

    im going to work on the task model, we need sanitization and filters

--- taskManager/src/db/mongoose.js
+++ taskManager/src/db/mongoose.js
@@ -6,61 +6,61 @@ mongoose.connect('mongodb://127.0.1:27017/taskManagerApi', {
     //this ensures data is accessible
     useCreateIndex:true
 })
-//Takes two arguments(stringNameOfModel,definition-the fields we want)
-const User = mongoose.model(('User'), {
-    //Set:{valueAnObject}
-    name:{
-        type:String,
-        required:true,
-        trim: true
-    },
-    email:{
-        type:String,
-        required:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Thats not a valid email')
-            }
-        },
-        trim:true,
-        lowercase:true
+// //Takes two arguments(stringNameOfModel,definition-the fields we want)
+// const User = mongoose.model(('User'), {
+//     //Set:{valueAnObject}
+//     name:{
+//         type:String,
+//         required:true,
+//         trim: true
+//     },
+//     email:{
+//         type:String,
+//         required:true,
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 throw new Error('Thats not a valid email')
+//             }
+//         },
+//         trim:true,
+//         lowercase:true
 
-    },
-    age:{
-        type:Number,
-        default: 0,
-        validate(value){
-            if (value < 0){
-                throw new Error('Your Age Needs to be a real number')
-            }
+//     },
+//     age:{
+//         type:Number,
+//         default: 0,
+//         validate(value){
+//             if (value < 0){
+//                 throw new Error('Your Age Needs to be a real number')
+//             }
 
-        }
-    },
-    password:{
-        type:String,
-        required: true,
-        trim:true,
-        minLength: 7,
-        validate(value){
-            if (value.toLowerCase().includes('password')){
-                throw new Error ('Password cant contain "password"')
-            }
-        }
-    }
-})
+//         }
+//     },
+//     password:{
+//         type:String,
+//         required: true,
+//         trim:true,
+//         minLength: 7,
+//         validate(value){
+//             if (value.toLowerCase().includes('password')){
+//                 throw new Error ('Password cant contain "password"')
+//             }
+//         }
+//     }
+// })
 
-const me = new User ({
+// const me = new User ({
 
-    name:'Hugo    ',
-    email:'Make@gmail.COM   ',
-    password:'Phone898!'
-})
+//     name:'Hugo    ',
+//     email:'Make@gmail.COM   ',
+//     password:'Phone898!'
+// })
 
-me.save().then((me) => {
-    console.log(me)
-}).catch((error) =>{
-    console.log('There was an error' + error)
-})
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((error) =>{
+//     console.log('There was an error' + error)
+// })
 
 const Task = mongoose.model(('tasks'), {
     description:{
@@ -71,13 +71,13 @@ const Task = mongoose.model(('tasks'), {
     }
 })
 
-// const task = new Task({
-//     description:'This is my first Task',
-//     completed:false
-// })
+const task = new Task({
+    description:'This is my first Task',
+    completed:false
+})
 
-// task.save().then((task) => {
-//     console.log(task)
-// }).catch((error) => {
-//     console.log(error)
-// })
+task.save().then((task) => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
+})
