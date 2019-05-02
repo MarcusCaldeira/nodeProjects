901fbf4a9784b43db22eab5b12e4db9de70a8f7e
MarcusCaldeira <mac.caldr@gmail.com>
2019-05-02 16:01:04 -0500

    ok im gonna explore heroko for the rest of the day. Im pretty sure that express is configured

--- taskManager/src/index.js
+++ taskManager/src/index.js
@@ -1,1 +1,10 @@
 //the starting point for the application via express (initialize the express server)
+const express = require('express')
+
+const app = express()
+const port = process.env.port || 3000
+
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
+
+})
