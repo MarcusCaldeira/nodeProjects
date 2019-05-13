8dd37ca64ed343bbbcc399dd40a832e3e8b9edc7
MarcusCaldeira <mac.caldr@gmail.com>
2019-05-13 12:33:33 -0500

    .use will let parse through the json and return it to the console.

--- taskManager/src/index.js
+++ taskManager/src/index.js
@@ -4,8 +4,10 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
+app.use(express.json())
 //Same exact call signature... 1. The Path 2. The function that runs when someone tries to access a particular route.
 app.post('/users', (req,res) => {
+    console.log(req.body)
     res.send('Testing!')
 
 })
