824111ae59f8bc261088f6c18b8220ad727dbe80
MarcusCaldeira <mac.caldr@gmail.com>
2019-05-13 12:28:07 -0500

    need to make sure that my get/posting methods are correct inside of postman otherwise. ill get status 404

--- taskManager/src/index.js
+++ taskManager/src/index.js
@@ -4,8 +4,11 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
+//Same exact call signature... 1. The Path 2. The function that runs when someone tries to access a particular route.
+app.post('/users', (req,res) => {
+    res.send('Testing!')
 
-app.post 
+})
 
 app.listen(port, () => {
     console.log('Server is up on port' + port)
